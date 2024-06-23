1fbc2e8973cebe9e180458fdf2c08a423919729e
jungkko <kim35990960@gmail.com>
2024-06-23 22:19:28 +0900

    스크립트 주석 추가

--- Assets/2.Scripts/Dice_Countroll.cs
+++ Assets/2.Scripts/Dice_Countroll.cs
@@ -13,11 +13,11 @@ public class Dice_Countroll : MonoBehaviour
     }
     public void Dice_Shoot()
     {
-        Vector3 speed = new Vector3(500, 500, 500);
+        Vector3 speed = new Vector3(500, 500, 500);//주사위가 받는 힘
 
-        dice1.transform.position = new Vector3(-30, 10, -32.5f);
-        dice1.transform.rotation = Quaternion.Euler(Random.Range(0.0f,30f), Random.Range(0.0f, 30f), Random.Range(0.0f, 30f));
-        dice1.GetComponent<Rigidbody>().AddForce(speed);
+        dice1.transform.position = new Vector3(-30, 10, -32.5f);//주사위가 잘 던져지게 위치이동
+        dice1.transform.rotation = Quaternion.Euler(Random.Range(0.0f,30f), Random.Range(0.0f, 30f), Random.Range(0.0f, 30f));//주사위를 랜덤으로 회전시킨다.
+        dice1.GetComponent<Rigidbody>().AddForce(speed);//주사위의 리지드바디에 힘을 줌
 
         dice2.transform.position = new Vector3(-30, 11, -32.5f);
         dice2.transform.rotation = Quaternion.Euler(Random.Range(0.0f, 30f), Random.Range(0.0f, 30f), Random.Range(0.0f, 30f));
