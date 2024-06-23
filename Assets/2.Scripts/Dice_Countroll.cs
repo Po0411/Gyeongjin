e9bc483ead1888b205ad2cf53c11fb22cf778df0
jungkko <kim35990960@gmail.com>
2024-06-23 22:52:56 +0900

    주사위 딜레이 추가

--- Assets/2.Scripts/Dice_Countroll.cs
+++ Assets/2.Scripts/Dice_Countroll.cs
@@ -6,10 +6,17 @@ public class Dice_Countroll : MonoBehaviour
 {
     public GameObject dice1;
     public GameObject dice2;
+    public float delay_time_num;
+    float delay_time;
 
     public void Onclick()
     {
-        Dice_Shoot();
+        if (delay_time <= 0)
+        {
+            Dice_Shoot();
+            delay_time = delay_time_num;
+        }
+
     }
     public void Dice_Shoot()
     {
@@ -22,5 +29,11 @@ public class Dice_Countroll : MonoBehaviour
         dice2.transform.position = new Vector3(-30, 11, -32.5f);
         dice2.transform.rotation = Quaternion.Euler(Random.Range(0.0f, 30f), Random.Range(0.0f, 30f), Random.Range(0.0f, 30f));
         dice2.GetComponent<Rigidbody>().AddForce(speed);
+    }
+    public void Update()
+    {
+        delay_time-= Time.deltaTime;
+        if (delay_time <= 0)
+            Debug.Log("주사위 완료");
     }
 }
