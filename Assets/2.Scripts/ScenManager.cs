6000c1cfd6141e825f3b7c11b2559f41b5ef4c5a
조하랑 <wawa111027@gmail.com>
2024-06-23 20:54:35 +0900

    뒤끝 sdk 업데이트

--- Assets/2.Scripts/ScenManager.cs
+++ Assets/2.Scripts/ScenManager.cs
@@ -11,7 +11,7 @@ public class ScenManager : MonoBehaviour
     {
         if (other.CompareTag("Player")) // 플레이어 태그 값으로 확인하고 이동
         {
-            Debug.Log("플레이어가 정상적으로" + Scene + "으로 이동 했습니다"); // 콘솔창으로 확인
+            Debug.Log("플레이어가 정상적으로" + Scene + "(으)로 이동 했습니다"); // 콘솔창으로 확인
             SceneManager.LoadScene(Scene); // 입력 씬으로 이동
         }
     }
