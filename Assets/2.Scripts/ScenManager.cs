6a5a4ae7c0161fa53817d1fa8183685e6d414a49
조하랑 <wawa111027@gmail.com>
2024-07-18 23:37:55 +0900

    회원가입 제작 완료

--- Assets/2.Scripts/ScenManager.cs
+++ Assets/2.Scripts/ScenManager.cs
@@ -7,12 +7,17 @@ public class ScenManager : MonoBehaviour
 {
     public string Scene; // 유니티 창에서 씬 이름 입력
 
-    void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // 플레이어 태그 값으로 확인하고 이동
         {
             Debug.Log("플레이어가 정상적으로" + Scene + "(으)로 이동 했습니다"); // 콘솔창으로 확인
             SceneManager.LoadScene(Scene); // 입력 씬으로 이동
         }
+    }
+
+    public void Scene_Button()
+    {
+        SceneManager.LoadScene(Scene); // 입력 씬으로 이동
     }
 }
